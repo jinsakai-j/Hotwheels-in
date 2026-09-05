1ac3f3db3b63a0821fbbe8ad6b93258c138e03aa
Rajaa Azharul <azharulrajaa@gmail.com>
2026-09-05 23:42:22 +0700

    Update katalog via admin

--- items.js
+++ items.js
@@ -5,5 +5,16 @@
 
 
 const items = [
-  {"name": "1", "series": "1", "price": 1, "status": "available", "image": "", "condition": "1", "year": "1", "stock": 1, "new": true},
+  {"name": "Item 1", "series": "", "price": "150000", "status": "available", "image": "", "condition": "Mint", "year": "2024", "stock": 1, "new": true},
+  {"name": "Item 2", "series": "", "price": "95000", "status": "available", "image": "", "condition": "Mint", "year": "2023", "stock": 1, "new": true},
+  {"name": "Item 3", "series": "", "price": "", "status": "available", "image": "", "condition": "Good", "year": "", "stock": 1, "new": true},
+  {"name": "Item 4", "series": "", "price": "120000", "status": "available", "image": "", "condition": "Mint", "year": "", "stock": 1},
+  {"name": "Item 5", "series": "", "price": "", "status": "available", "image": "", "condition": "Mint", "year": "", "stock": 1},
+  {"name": "Item 6", "series": "", "price": "80000", "status": "available", "image": "", "condition": "Good", "year": "2022", "stock": 1},
+  {"name": "Item 7", "series": "", "price": "260000", "status": "available", "image": "", "condition": "Mint", "year": "", "stock": 1},
+  {"name": "Item 8", "series": "", "price": "", "status": "available", "image": "", "condition": "Mint", "year": "", "stock": 1},
+  {"name": "Item 9", "series": "", "price": "175000", "status": "sold", "image": "", "condition": "Mint", "year": "", "stock": 0},
+  {"name": "Item 10", "series": "", "price": "110000", "status": "sold", "image": "", "condition": "Good", "year": "2021", "stock": 0},
+  {"name": "1", "series": "1", "price": 1, "status": "available", "image": "", "condition": "1", "year": "1", "stock": 1, "new": false},
+  {"name": "1", "series": "1", "price": 1, "status": "available", "image": "", "condition": "1", "year": "1", "stock": 1, "new": false}
 ];
