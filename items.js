01d9b803572bf126587cae5552ec0532c3ef2247
Rajaa Azharul <azharulrajaa@gmail.com>
2026-09-06 16:50:43 +0700

    Set all items condition to Good

--- items.js
+++ items.js
@@ -5,7 +5,7 @@
 
 
 const items = [
-  {"name": "'89 Mercedes-Benz 560 SEC AMG", "series": "Mainline - HW Turbo (2023)", "price": "60000", "status": "available", "image": "images/mercy.jpg", "condition": "Mint", "stock": 1, "new": true},
-  {"name": "Ford GT40", "series": "Silver Series - National Icons (HRT81, 1/5)", "price": "75000", "status": "available", "image": "images/ford.jpg", "condition": "Mint", "stock": 1, "new": true},
-  {"name": "Bugatti Veyron", "series": "Silver Series - Fast & Furious Racing Course (HNR88, 5/5)", "price": "90000", "status": "available", "image": "images/bugati.jpg", "condition": "Mint", "stock": 1, "new": true}
+  {"name": "'89 Mercedes-Benz 560 SEC AMG", "series": "Mainline - HW Turbo (2023)", "price": "60000", "status": "available", "image": "images/mercy.jpg", "condition": "Good", "stock": 1, "new": true},
+  {"name": "Ford GT40", "series": "Silver Series - National Icons (HRT81, 1/5)", "price": "75000", "status": "available", "image": "images/ford.jpg", "condition": "Good", "stock": 1, "new": true},
+  {"name": "Bugatti Veyron", "series": "Silver Series - Fast & Furious Racing Course (HNR88, 5/5)", "price": "90000", "status": "available", "image": "images/bugati.jpg", "condition": "Good", "stock": 1, "new": true}
 ];
